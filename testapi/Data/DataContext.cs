d42f51e5492cc73f6f732c713b4d5cde009fa6a6
CapnPrice <Robertprice12300@Gmail.com>
2023-01-06 02:22:28 -0800

    first commit

---
+++ testapi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testapi.Models;
+
+namespace testapi.Data
+{
+    public class DataContext : DbContext
+        
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ testapi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace testapi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
